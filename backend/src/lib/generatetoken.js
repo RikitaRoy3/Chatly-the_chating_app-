205c2ea70bdc981ce432db7f7efb2d328dca23f3
Rikita Roy <rikitaroy21@gmail.com>
2026-02-01 02:38:47 +0530

    code refactoring

--- backend/src/lib/generatetoken.js
+++ backend/src/lib/generatetoken.js
@@ -14,8 +14,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 1 * 60 * 60 * 1000, // 1 hrs
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "none", // CSRF attacks
-    secure: true,
+    sameSite: "lax", // CSRF attacks
+    secure: false,
   });
   return token;
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -31,7 +31,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));// This will prevent PayloadTooLargeError(This means request of size upto 10mb can come from the frontend)
 app.use(cors({
   origin: [
-    " http://localhost:5173",
+    "http://localhost:5173",
 
     "http://localhost:8080",
 
